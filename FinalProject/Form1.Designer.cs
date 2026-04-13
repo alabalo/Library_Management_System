51d243bb2b316306f119fb49b6d72222df8df7ed
alabalo <dinobaylon@gmail.com>
2026-04-13 16:04:10 +0800

    do

--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -779,7 +779,6 @@
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Borrowed Books";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
